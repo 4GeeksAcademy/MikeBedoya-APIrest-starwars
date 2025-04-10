a7d04506bd989d17981a12e56455b5fb1fdd5512
Mike Bedoya <mikebedoya28@gmail.com>
2025-04-10 15:45:08 +0000

    finished API Rest

--- src/app.py
+++ src/app.py
@@ -8,43 +8,132 @@ from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
-from models import db, User
+from models import db, User, People, Planet, Favorites
 #from models import Person
-
 app = Flask(__name__)
 app.url_map.strict_slashes = False
-
 db_url = os.getenv("DATABASE_URL")
 if db_url is not None:
     app.config['SQLALCHEMY_DATABASE_URI'] = db_url.replace("postgres://", "postgresql://")
 else:
     app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:////tmp/test.db"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
 MIGRATE = Migrate(app, db)
 db.init_app(app)
 CORS(app)
 setup_admin(app)
-
 # Handle/serialize errors like a JSON object
 @app.errorhandler(APIException)
 def handle_invalid_usage(error):
     return jsonify(error.to_dict()), error.status_code
-
 # generate sitemap with all your endpoints
 @app.route('/')
 def sitemap():
     return generate_sitemap(app)
-
-@app.route('/user', methods=['GET'])
-def handle_hello():
-
-    response_body = {
-        "msg": "Hello, this is your GET /user response "
-    }
-
-    return jsonify(response_body), 200
-
+# @app.route('/user', methods=['GET'])
+# def handle_hello():
+#     response_body = {
+#         "msg": "Hello, this is your GET /user response "
+#     }
+#     return jsonify(response_body), 200
+@app.route('/people', methods=['GET'])
+def get_all_people():
+    all_people = People.query.all()
+    print(all_people)
+    results = list(map(lambda people: people.serialize(), all_people))
+    print(results)
+    # response_body = {
+    #     "msg": "Hello, this is your GET /people response ",
+    #     "results": results
+    # }
+    return jsonify(results), 200               #se vuoi che appaia response_body lo metti tra parentesi, altrimenti metti direttamente "results" che è la variabile che racchiude la lista
+@app.route('/people/<int:people_id>', methods=['GET'])
+def get_single_people(people_id):
+    single_people = People.query.get(people_id)
+    print(single_people.serialize())
+    return jsonify(single_people.serialize()), 200
+@app.route('/planets', methods=['GET'])
+def get_all_planets():
+    all_planets = Planet.query.all()
+    print(all_planets)
+    results2 = list(map(lambda planet: planet.serialize(), all_planets))
+    print(results2)
+    # response_body = {
+    #     "msg": "Hello, this is your GET /people response ",
+    #     "results": results
+    # }
+    return jsonify(results2), 200
+@app.route('/planets/<int:planet_id>', methods=['GET'])
+def get_single_planet(planet_id):
+    single_planet = Planet.query.get(planet_id)
+    print(single_planet.serialize())
+    return jsonify(single_planet.serialize()), 200
+@app.route('/users', methods=['GET'])
+def get_all_users():
+    all_planets = User.query.all()
+    print(all_planets)
+    results_planets = list(map(lambda planet: planet.serialize(), all_planets))
+    return jsonify(results_planets), 200
+@app.route('/users/favorites', methods=['GET'])
+def get_all_favorites():
+    all_favorites = Favorites.query.all()
+    print(all_favorites)
+    results_favorites = list(map(lambda fav: fav.serialize(), all_favorites))
+    return jsonify(results_favorites), 200
+@app.route('/favorites/planet/<int:planet_id>', methods=['POST'])
+def add_planet_to_favorites( planet_id):
+    print(request)                      #request è un oggetto di Flask che contiene tutte le informazioni relative alla richiesta dal client
+    print(request.json)
+    user = User.query.first()    # Recupera il primo utente dal database
+    planet = Planet.query.get(planet_id)   # Recupera il pianeta dal database (controlla se esiste), si potrebbe fare anche direttamente con l'URL senza questo passaggio e nel fav metto planet_id = planet_id
+    if not planet :
+        return jsonify({"error" : "Planet not found"}), 404
+    new_fav = Favorites(user_id= user.id, planet_id = planet.id, planet_name = planet.name)
+    db.session.add(new_fav)
+    db.session.commit()
+    return jsonify({"message": "planet successfully added"}), 200
+@app.route('/favorites/planet/<int:planet_id>', methods=['DELETE'])
+def delete_planet_to_favorites(planet_id):
+    print(request)                      #request è un oggetto di Flask che contiene tutte le informazioni relative alla richiesta dal client
+    print(request.json)
+    user = User.query.first()
+    planet = Planet.query.get(planet_id)
+    if not planet :
+        return jsonify({"error" : "Planet not found"}), 404
+    fav_to_delete = Favorites.query.filter_by(user_id= user.id, planet_id = planet.id).first()    # Trova il record di favorite da eliminare
+    print("Planet ID:", planet.id)
+    if not fav_to_delete :
+        return jsonify({"error": "Favorite not found"}), 404
+    db.session.delete(fav_to_delete)
+    db.session.commit()
+    return jsonify({"message" : "favorite planet successfully deleted"}), 200
+@app.route('/favorites/people/<int:people_id>', methods=['POST'])
+def add_people_to_favorites(people_id):
+    print(request)                      #request è un oggetto di Flask che contiene tutte le informazioni relative alla richiesta dal client
+    print(request.json)
+    user = User.query.first()    # Recupera il primo utente dal database
+    people = People.query.get(people_id)   # Recupera il personaggio dal database (controlla se esiste), si potrebbe fare anche direttamente con l'URL senza questo passaggio e nel fav metto people_id = people_id
+    if not people :
+        return jsonify({"error" : "Character not found"}), 404
+    new_fav = Favorites(user_id= user.id, people_id = people.id, people_name = people.name)
+    db.session.add(new_fav)
+    db.session.commit()
+    return jsonify({"message": "character successfully added"}), 200
+@app.route('/favorites/people/<int:people_id>', methods=['DELETE'])
+def delete_people_to_favorites(people_id):
+    print(request)                      #request è un oggetto di Flask che contiene tutte le informazioni relative alla richiesta dal client
+    print(request.json)
+    user = User.query.first()
+    people = People.query.get(people_id)
+    if not people :
+        return jsonify({"error" : "people not found"}), 404
+    fav_to_delete = Favorites.query.filter_by(user_id= user.id, people_id = people.id).first()    # Trova il record di favorite da eliminare
+    print("People ID:", people.id)
+    if not fav_to_delete :
+        return jsonify({"error": "Favorite not found"}), 404
+    db.session.delete(fav_to_delete)
+    db.session.commit()
+    return jsonify({"message" : "favorite people successfully deleted"})
 # this only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
     PORT = int(os.environ.get('PORT', 3000))
--- src/models.py
+++ src/models.py
@@ -1,19 +1,67 @@
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import String, Boolean
-from sqlalchemy.orm import Mapped, mapped_column
 
 db = SQLAlchemy()
-
 class User(db.Model):
-    id: Mapped[int] = mapped_column(primary_key=True)
-    email: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
-    password: Mapped[str] = mapped_column(nullable=False)
-    is_active: Mapped[bool] = mapped_column(Boolean(), nullable=False)
-
-
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String(80), unique=False, nullable=False)
+    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    favorites = db.relationship("Favorites", backref='user')
+    def __repr__(self):
+        return '<User %r>' % self.email
     def serialize(self):
         return {
-            "id": self.id,
+            "id": self.id,          #self si usa per accedere agli attributi(colonne) di User, in questo caso l'id e la mail
             "email": self.email,
             # do not serialize the password, its a security breach
+        }
+class People(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120), unique=True, nullable=False)
+    gender = db.Column(db.String(80), unique=False, nullable=False)
+    eye_color = db.Column(db.String(80), unique=False, nullable=False)
+    hair_color = db.Column(db.String(80), unique=False, nullable=False)
+    favorites = db.relationship("Favorites", backref='people')
+    def __repr__(self):                                                    #come mi riferisco al elemento del modello
+        return '<People %r>' % self.name
+    def serialize(self):
+        return {                                                          #ciò che voglio mostrare e lo mostro come dizionario
+            "id": self.id,
+            "name": self.name,
+            "gender": self.gender,
+            "eye_color" : self.eye_color,
+            "hair_color" : self.hair_color
+        }
+class Planet(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120), unique=True, nullable=False)
+    climate = db.Column(db.String(80), unique=False, nullable=False)
+    population = db.Column(db.Integer, unique=False, nullable=False)
+    terrain = db.Column(db.String(80), unique=False, nullable=False)
+    favorites = db.relationship("Favorites", backref='planet')
+    def __repr__(self):                                                    #come mi riferisco al elemento del modello
+        return '<Planet %r>' % self.name
+    def serialize(self):
+        return {                                                          #ciò che voglio mostrare e lo mostro come dizionario
+            "id": self.id,
+            "name": self.name,
+            "climate": self.climate,
+            "population" : self.population,
+            "terrain" : self.terrain
+        }
+class Favorites(db.Model):
+    __tablename__ = 'favorites'
+    id = db.Column(db.Integer, primary_key=True)
+    people_name = db.Column(db.String(120), nullable=True)
+    planet_name = db.Column(db.String(120), nullable=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=True)
+    people_id = db.Column(db.Integer, db.ForeignKey('people.id'), nullable=True)
+    planet_id = db.Column(db.Integer, db.ForeignKey('planet.id'), nullable=True)
+    def __repr__(self):                                                    #come mi riferisco al elemento del modello
+        return '<Favorites %r>' % self.id
+    def serialize(self):
+        return {                                                          #ciò che voglio mostrare e lo mostro come dizionario
+            "id": self.id,
+            "people_name": self.people.name if self.people else None,
+            "planet_name": self.planet.name if self.planet else None
         }
